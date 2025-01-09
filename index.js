e4f435899db155e40e4769300aeafa25e762407c
naitsircnat <cristiangerardchen@gmail.com>
2025-01-09 03:36:50 +0000

    Added start script to package.json file

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ async function main() {
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
   });
 
   // VIEW PRODUCTS
